7281930e1aa3376ec0a64f900f7e570c1abf4e3a
Miyazawa Akira <pecorarista@gmail.com>
2016-11-26 04:18:17 +0900

    Add post

--- gulpfile.js
+++ gulpfile.js
@@ -8,32 +8,40 @@ const sasslint = require('gulp-sass-lint')
 const postcss = require('gulp-postcss');
 const cssnext = require('postcss-cssnext');
 const processors = [ cssnext() ];
+const dist = 'dist';
+const pugFiles = ['pug/**/*.pug', '!pug/**/_*.pug'];
+const sassFiles = ['sass/**/*.s+(a|c)ss'];
+const highlighter = {
+  css: 'bower_components/highlightjs/styles/solarized-light.css',
+  js: 'bower_components/highlightjs/highlight.pack.js'
+};
+const mathjax = 'bower_components/MathJax/MathJax.js';
 
-const pugFiles = ['pug/**/*.pug', '!pug/**/_*.pug']
-const sassFiles = ['sass/**/*.s+(a|c)ss']
-
-gulp.task('pug', () =>
-  gulp.src(pugs)
+gulp.task('pug', () => {
+  gulp.src(pugFiles)
     .pipe(puglint())
     .pipe(puglint.reporter('fail'))
     .pipe(pug({
-      pretty: true,
-      data: {
-        lang: 'en'
-      }
+      pretty: true
     }))
-    .pipe(gulp.dest('html'))
-);
+    .pipe(gulp.dest('dist'));
+});
 
 gulp.task('sass', () =>
-  gulp.src(stylesheets)
+  gulp.src(sassFiles)
     .pipe(sasslint())
     .pipe(sasslint.format())
     .pipe(sasslint.failOnError())
     .on('error', error => process.exit(error))
     .pipe(sass({ outputStyle: 'expanded' }))
     .pipe(postcss(processors))
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('dist/css'))
 );
 
-gulp.task('default', () => ['pug', 'sass']);
+gulp.task('vendor', () => {
+  gulp.src(highlighter.css).pipe(gulp.dest('dist/css'));
+  gulp.src(highlighter.js).pipe(gulp.dest('dist/js'));
+  gulp.src(mathjax).pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', ['pug', 'sass', 'vendor']);
