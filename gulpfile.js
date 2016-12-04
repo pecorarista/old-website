5339f3f849222488a1c09fd78feac76818b61b93
Miyazawa Akira <pecorarista@gmail.com>
2016-12-04 15:37:06 +0900

    Update index

--- gulpfile.js
+++ gulpfile.js
@@ -10,32 +10,15 @@ const postcss = require('gulp-postcss');
 const cssnext = require('postcss-cssnext');
 const processors = [ cssnext() ];
 const dist = 'dist';
-const pugFiles = ['pug/**/*.pug', '!pug/**/_*.pug'];
+const posts = require('./posts.json');
+const pugFiles = ['pug/**/*.pug', '!pug/**/_*.pug', '!pug/index.pug'];
+const index = 'pug/index.pug';
 const sassFiles = ['sass/**/*.s+(a|c)ss'];
-const highlighter = {
-  css: 'bower_components/highlightjs/styles/solarized-light.css',
-  js: 'bower_components/highlightjs/highlight.pack.js'
-};
-const mathjax = 'bower_components/MathJax/**/*.js';
-const materialize = {
-  css: 'bower_components/materialize/dist/css/materialize.css',
-  js: 'bower_components/materialize/dist/js/materialize.js'
-};
-const roboto = {
-  css: 'bower_components/roboto-fontface/css/**/*.css',
-  font: 'bower_components/roboto-fontface/fonts/**/*.{eot,svg,woff,ttf,woff2}'
-};
-const jquery = 'bower_components/jquery/dist/jquery.min.js';
 const img = 'img/**/*';
 
 gulp.task('img', () =>
   gulp.src(img).pipe(gulp.dest('dist/public/img/'))
 );
-
-gulp.task('font', () => {
-  gulp.src(roboto.font).pipe(gulp.dest('dist/public/fonts/'));
-  gulp.src(roboto.css).pipe(gulp.dest('dist/public/css/'));
-});
 
 gulp.task('pug', () => {
   gulp.src(pugFiles)
@@ -58,19 +41,27 @@ gulp.task('sass', () =>
     .pipe(gulp.dest('dist/public/css'))
 );
 
-gulp.task('vendor', () => {
-  gulp.src(highlighter.css).pipe(gulp.dest('dist/public/css'));
-  gulp.src(highlighter.js).pipe(gulp.dest('dist/public/js'));
-  gulp.src(mathjax).pipe(gulp.dest('dist/public/js'));
-  gulp.src(materialize.css).pipe(gulp.dest('dist/public/css'));
-  gulp.src(materialize.js).pipe(gulp.dest('dist/public/js'));
-  gulp.src(jquery).pipe(gulp.dest('dist/public/js'));
+gulp.task('index', () => {
+  ['en', 'ja'].forEach(lang =>
+    gulp.src(index)
+      .pipe(puglint())
+      .pipe(puglint.reporter('fail'))
+      .pipe(pug({
+        pretty: true,
+        data: {
+          lang: lang,
+          posts: posts
+        }
+      }))
+      .pipe(gulp.dest('dist'))
+  );
 });
 
-gulp.task('default', ['img', 'font', 'pug', 'sass', 'vendor']);
+gulp.task('default', ['img', 'index', 'pug', 'sass']);
 
 gulp.task('server', ['default'],() => {
-  gulp.watch(pugFiles, ['pug']);
+  gulp.watch(['pug/**/*.pug', '!pug/index.pug'], ['pug']);
+  gulp.watch([index, 'pug/**/_*.pug'], ['index']);
   gulp.watch(sassFiles, ['sass']);
   gulp.src(dist)
     .pipe(webserver({
